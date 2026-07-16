fa15d706a2f0b5b20b2fb95080eb114da0304b04
row-huh <rohaabropathan@gmail.com>
2026-07-16 14:02:35 +0500

    added task descriptions

--- main.py
+++ main.py
@@ -12,18 +12,38 @@ app = FastAPI()
 
 @app.get('/')
 def root():
-    return {"name": "TaskAPI", "version": "1.0.0"}
+    """
+    Initial information about the API, including its name, version, and available endpoints.
+    """
+    return {"name": "TaskAPI", "version": "1.0.0", "Endpoints": {
+        "GET (/)": "Get initial information about the API",
+        "GET (/health)": "Check the health of the API",
+        "GET (/tasks)": "Get all tasks",
+        "GET (/tasks/{task_id})": "Get a specific task by ID",
+        "POST (/tasks)": "Add a new task",
+        "PUT (/tasks/{task_id})": "Update an existing task",
+        "DELETE (/tasks/{task_id})": "Delete a task"
+    }}
 
 @app.get('/health')
 def health():
+    """
+    Status Check
+    """
     return {"status": "ok"}
 
 @app.get('/tasks')
 def get_tasks():
+    """
+    Retrieve all tasks.
+    """
     return tasks
 
 @app.get('/tasks/{task_id}')
 def get_task(task_id: int):
+    """
+    Get task by an ID. If the task is not found, return an error message.
+    """
     task = next((task for task in tasks if task["id"] == task_id), None)
     if task:
         return task
@@ -32,6 +52,9 @@ def get_task(task_id: int):
 
 @app.post('/tasks/{title}')
 def add_task( title:str ):
+    """
+    Add a new task.
+    """
     if not title.strip():
         raise HTTPException(status_code=400, detail="Title cannot be empty")
 
@@ -48,7 +71,10 @@ def add_task( title:str ):
 
 # put tasks 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int, updated: TaskUpdate):
+def update_task(task_id: int, updated: dict):
+    """
+    Update an existing task
+    """
     task = next((t for t in tasks if t["id"] == task_id), None)
 
     if task is None:
@@ -69,3 +95,5 @@ def update_task(task_id: int, updated: TaskUpdate):
         task["completed"] = updated.completed
 
     return task
+
+
