e8e0c8bfb87e4a6889b442bd3c64f0da84676472
row-huh <rohaabropathan@gmail.com>
2026-07-16 13:47:32 +0500

    root route, health and fetching task_id completed

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
+from http.client import HTTPException
+
 from fastapi import FastAPI
 
 tasks = [
-    {"id": 1, "title": "Task 1", "description": "This is task 1", "completed": False},
-    {"id": 2, "title": "Task 2", "description": "This is task 2", "completed": True},
-    {"id": 3, "title": "Task 3", "description": "This is task 3", "completed": False}
+    {"id": 1, "title": "Task 1", "completed": False},
+    {"id": 2, "title": "Task 2", "completed": True},
+    {"id": 3, "title": "Task 3", "completed": False}
 ]
 
 app = FastAPI()
 
-@app.get('/'): 
-    #TODO return details
+@app.get('/')
+def root():
+    return {"name": "TaskAPI", "version": "1.0.0"}
 
-@app.get('/health'):
-    return {"status": "healthy"}
+@app.get('/health')
+def health():
+    return {"status": "ok"}
 
-@app.get('/tasks'):
+@app.get('/tasks')
+def get_tasks():
     return tasks
 
+@app.get('/tasks/{task_id}')
+def get_task(task_id: int):
+    task = next((task for task in tasks if task["id"] == task_id), None)
+    if task:
+        return task
+    return {"error": "Task not found"}
 
+
+@app.post('/tasks/{title}')
+def add_task( title:str ):
+    if not title.strip():
+        raise HTTPException(status_code=400, detail="Title cannot be empty")
+
+
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": title,
+        "completed": False
+    }
+    tasks.append(new_task)
+    return new_task
